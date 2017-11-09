fc5073c7a50e3e6f9b25a93de1cd5e32865679b1
wizera <wizera@users.noreply.github.com>
2017-11-09 20:26:20 +0100

    update by wizera

--- SteamGroups.cs
+++ SteamGroups.cs
@@ -1,16 +1,19 @@
-﻿/*
+/*
  * TODO: Add queue for old member removal
  */
 
+using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 using Oxide.Core.Plugins;
 using Oxide.Core.Libraries.Covalence;
 
+using System.Linq;
+
 namespace Oxide.Plugins
 {
-    [Info("Steam Groups", "Wulf/lukespragg", "0.3.8", ResourceId = 2085)]
+    [Info("Steam Groups", "Wulf/lukespragg", "0.3.7", ResourceId = 2085)]
     [Description("Automatically adds members of Steam group(s) to a permissions group")]
     public class SteamGroups : CovalencePlugin
     {
@@ -24,6 +27,8 @@ namespace Oxide.Plugins
         private readonly Regex idRegex = new Regex(@"<steamID64>(?<id>.+)</steamID64>");
         private readonly Regex pageRegex = new Regex(@"<currentPage>(?<page>.+)</currentPage>");
         private readonly Regex pagesRegex = new Regex(@"<totalPages>(?<pages>.+)</totalPages>");
+
+        private Boolean backoffPoll = false;
 
         private class Member
         {
@@ -82,7 +87,12 @@ namespace Oxide.Plugins
                 if (!permission.GroupExists(group.Oxide)) permission.CreateGroup(group.Oxide, group.Oxide, 0);
                 AddGroup(group.Steam, group.Oxide);
             }
-            GetMembers();
+
+            // Start the timed worker (only once, will be enabled entire plugin lifecycle)
+            InitializeTimedSteamMemberCheckWorker();
+
+            // Trigger check of Steam members immediately
+            QueueWorkerThread(worker => CheckSteamGroupsForMembers());
         }
 
         #endregion
@@ -101,11 +111,21 @@ namespace Oxide.Plugins
 
         private void ProcessQueue()
         {
-            var member = membersQueue.Dequeue();
-            if (permission.UserHasGroup(member.Id, groups[member.Group])) return;
+            QueueWorkerThread(worker =>
+            {
+                try
+                {
+                    var member = membersQueue.Dequeue();
+                    if (permission.UserHasGroup(member.Id, groups[member.Group])) return;
 
-            permission.AddUserGroup(member.Id, groups[member.Group]);
-            //Puts($"{member.Id} from {member.Group} added to '{groups[member.Group]}' group");
+                    permission.AddUserGroup(member.Id, groups[member.Group]);
+                    Puts($"{member.Id} from {member.Group} added to '{groups[member.Group]}' group");
+                }
+                catch (Exception e)
+                {
+                    Puts("An error occurred while processing queue: " + e);
+                }
+            });
         }
 
         private void OnTick()
@@ -140,60 +160,129 @@ namespace Oxide.Plugins
 
         #region Member Grabbing
 
-        private void GetMembers()
-        {
-            foreach (var group in steamGroups) GetMembers(group.Value, group.Key);
-        }
-
-        private void GetMembers(string url, string group, int page = 1)
-        {
-            Puts($"Checking page {page} of members for Steam group '{group}'");
-
-            webrequest.EnqueueGet($"{url}&p={page}", (code, response) =>
-            {
-                if (code == 403 || code == 429)
-                {
-                    Puts($"Steam is currently not allowing connections from your server. ({code})");
-                    Puts("Trying again in 10 minutes...");
-                    timer.Once(600f, () => GetMembers(url, group, page));
-                    return;
-                }
-
-                if (code != 200 || response == null)
-                {
-                    Puts($"Checking for Steam group members failed! ({code})");
-                    Puts("Trying again in 10 minutes...");
-                    timer.Once(600f, () => GetMembers(url, group, page));
-                    return;
-                }
-
-                var ids = idRegex.Matches(response);
-                foreach (Match match in ids)
-                {
-                    var id = match.Groups["id"].Value;
-                    if (members.Contains(id)) continue;
-
-                    members.Add(id);
-                    membersQueue.Enqueue(new Member(id, group));
-                }
-
-                int currentPage;
-                int totalPages;
-                int.TryParse(pageRegex.Match(response)?.Groups[1]?.Value, out currentPage);
-                int.TryParse(pagesRegex.Match(response)?.Groups[1]?.Value, out totalPages);
-
-                if (currentPage != 0 && totalPages != 0 && currentPage < totalPages)
-                    GetMembers(url, group, currentPage + 1);
-                else
-                    timer.Once(config.UpdateInterval, () => GetMembers(url, group));
-            }, this);
-        }
-
         [Command("steammembers")]
         private void MembersCommand(IPlayer player, string command, string[] args)
         {
             player.Reply("Checking for new Steam group members...");
-            GetMembers();
+
+            // Trigger check of Steam members immediately
+            QueueWorkerThread(worker => CheckSteamGroupsForMembers());
+        }
+
+        private void InitializeTimedSteamMemberCheckWorker()
+        {
+            timer.Every(config.UpdateInterval, () =>
+            {
+                // There is no need to check if player count is 0
+                if (players.Connected.Count() <= 0) return;
+
+                // While backoff is enabled, we do not want to check members
+                if (backoffPoll)
+                {
+                    Puts("Currently in backoff state, will not poll steam groups for members");
+                    return;
+                };
+
+                try
+                {
+                    QueueWorkerThread(worker => CheckSteamGroupsForMembers());
+                }
+                catch (Exception e)
+                {
+                    Puts("Exception occurred: " + e);
+                }
+            });
+        }
+
+        private void CheckSteamGroupsForMembers()
+        {
+            try
+            {
+                Puts("Polling steam groups");
+                foreach (var group in steamGroups)
+                {
+                    int page = 1;
+                    //Puts("CheckRemoteMembers -> processing steam group: " + group.Key + ", baseUrl: " + group.Value + ", starting from page: " + page);
+
+                    // One group at the time, no additional threading, we don't want to flood the API
+                    ExecuteSteamMemberListHttpGetCall(group.Key, group.Value, page);
+                }
+            }
+            catch (Exception e)
+            {
+                Puts("Exception occurred: " + e);
+            }
+        }
+
+        private void ExecuteSteamMemberListHttpGetCall(string groupName, string baseUrl, int page)
+        {
+            try
+            {
+                // Custom timeout for HTTP call (in milliseconds)
+                float timeout = 10000f;
+
+                // Build base URL + page number and execute the request
+                string url = string.Format("{0}&p={1}", baseUrl, page);
+                webrequest.Enqueue(url, null, (code, response) => SteamMemberListCallback(code, response, groupName, baseUrl, groupName), this, Core.Libraries.RequestMethod.GET, null, timeout);
+            }
+            catch (Exception e)
+            {
+                Puts("Exception occurred: " + e);
+            }
+        }
+
+        private void SteamMemberListCallback(int code, string response, string group, string baseUrl, string groupName)
+        {
+            if (code == 403 || code == 429)
+            {
+                Puts(string.Format("Steam is currently not allowing connections from your server! code={0}. Aborting this call", code));
+                EnableBackoff();
+                return;
+            }
+
+            if (code != 200 || response == null)
+            {
+                Puts(String.Format("Checking for Steam group members failed! code={0}. Aborting this call", code));
+                EnableBackoff();
+                return;
+            }
+
+            var ids = idRegex.Matches(response);
+            foreach (Match match in ids)
+            {
+                var id = match.Groups["id"].Value;
+                if (members.Contains(id)) continue;
+
+                members.Add(id);
+                membersQueue.Enqueue(new Member(id, group));
+                //Puts("Adding " + id + " to " + group);
+            }
+
+            int currentPage;
+            int totalPages;
+            int.TryParse(pageRegex.Match(response)?.Groups[1]?.Value, out currentPage);
+            int.TryParse(pagesRegex.Match(response)?.Groups[1]?.Value, out totalPages);
+
+            if (currentPage != 0 && totalPages != 0 && currentPage < totalPages)
+            {
+                QueueWorkerThread(worker => ExecuteSteamMemberListHttpGetCall(groupName, baseUrl, currentPage + 1));
+            }
+        }
+
+        private void EnableBackoff()
+        {
+            if (!backoffPoll)
+            {
+                backoffPoll = true;
+                timer.Once(600f, () => QueueWorkerThread(worker => DisableBackoff()));
+                Puts("Backoff state enabled");
+            }
+        }
+
+        private void DisableBackoff()
+        {
+            Puts("Backoff state disabled");
+            backoffPoll = false;
         }
 
         #endregion
